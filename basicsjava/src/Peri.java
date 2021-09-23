45681e10b28ebe553080782e3af713c72ce729f9
Divysara <divyas94@gmail.com>
2021-09-23 15:59:09 -0500

    assignments class

---
+++ basicsjava/src/Peri.java
@@ -0,0 +1,13 @@
+public class Peri {
+    public static void main(String[] args) {
+        Triangle obj = new Triangle();
+        System.out.println();
+    }
+
+ class Triangle{
+    int a= 3, b=4, c=5;
+    int area = a*b*c;
+    int perimeter=a+b+c;
+    System.out.println();
+}
+}
